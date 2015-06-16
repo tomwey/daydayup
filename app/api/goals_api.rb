9e2ab6dc73b3c99fff28115ca4a96a665bb22e59
Tomwey <tomwey@163.com>
2015-06-16 22:42:21 +0800

    xxxx

--- app/api/goals_api.rb
+++ app/api/goals_api.rb
@@ -3,15 +3,15 @@
 module API
   class GoalsAPI < Grape::API
     
-    resource goals do
-      params do
-        requires :type_id, type: Integer, desc: "类别ID"
-      end
-      get do
-        @banners = Banner.where(category_id: params[:type_id]).order('id DESC')
-        
-        { code: 0, message: "ok", data: @banners }
-      end
+    resource :goals do
+      # params do
+      #   requires :type_id, type: Integer, desc: "类别ID"
+      # end
+      # get do
+      #   @banners = Banner.where(category_id: params[:type_id]).order('id DESC')
+      #   
+      #   { code: 0, message: "ok", data: @banners }
+      # end
     end # end resource 
     
   end
