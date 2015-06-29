edc991f5b6df23c67bda9a2e02e27e73fd05daa3
tomwey <tomwey@163.com>
2015-06-29 17:15:53 +0800

    xxxx

--- app/models/message.rb
+++ app/models/message.rb
@@ -12,7 +12,7 @@ class Message < ActiveRecord::Base
   def as_json(opts = {})
     {
       id: self.id,
-      content: self.body || "",
+      content: self.body || '',
       type: self.message_type,
       send_time: self.created_at.strftime('%Y-%m-%d %H:%M:%S'),
       actor: self.actor || {},
@@ -20,3 +20,5 @@ class Message < ActiveRecord::Base
   end
   
 end
+
+# body 如果是系统消息，则为消息的内容；如果是评论，字段的内容为目标id加当前评论的id，例如：'2-3'；如果是加油，那么字段内容为目标的id
