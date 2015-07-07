58c30ca3babb27eeffa37237e1f18e7366bdc417
Tomwey <tomwey@163.com>
2015-07-08 00:16:57 +0800

    xxxx

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :keep_releases, 5
 
 set :linked_files, %w{config/database.yml config/config.yml}
 
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads public/system}
 
 set(:symlinks, [
   {
