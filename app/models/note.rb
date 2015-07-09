e4e21b3e13769ba5bd224c633c99b917332239cf
Tomwey <tomwey@163.com>
2015-07-09 23:11:55 +0800

    xxxxx

--- app/models/note.rb
+++ app/models/note.rb
@@ -18,7 +18,9 @@ class Note < ActiveRecord::Base
       likes_count: self.likes_count,
       comments_count: self.comments_count,
       blike: self.blike || false,
-      goal_title: self.goal.try(:title) || "",
+      goal_title: self.title || "",
+      type: self.goal.category || {},
+      owner: self.goal.user || {},      
       published_at: self.created_at.strftime('%Y-%m-%d %H:%M:%S'),
     }
   end
